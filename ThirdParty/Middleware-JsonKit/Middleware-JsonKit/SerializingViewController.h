b7c066987a50cb3e207f682fc33ee2fb4ced6812
venkat kongara <v-vekong@microsoft.com>
2017-04-11 15:46:26 -0700

    changed the license headers

--- ThirdParty/Middleware-JsonKit/Middleware-JsonKit/SerializingViewController.h
+++ ThirdParty/Middleware-JsonKit/Middleware-JsonKit/SerializingViewController.h
@@ -1,10 +1,18 @@
+//******************************************************************************
 //
-//  SerializingViewController.h
-//  Middleware-JsonKit
+// Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 //
-//  Created by venkat kongara on 4/3/17.
-//  Copyright © 2017 microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
 
 #import <UIKit/UIKit.h>
 #import "ListViewController.h"
